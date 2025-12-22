9777eb333bbf8de99513728506fe2aa33afdfaa3
yanataka60 <92910500+yanataka60@users.noreply.github.com>
2025-12-22 11:49:17 +0900

    Add files via upload

---
+++ SAMPLE/EMMMENU/EMMMENU.s
@@ -0,0 +1,813 @@
+INPUTF		EQU	0003H	;１行入力
+PRINT		EQU	000BH	;文字列のプリント
+ACCPRT		EQU	0013H	;１文字出力
+INKEYD		EQU	001BH	;１文字入力
+CR2		EQU	04A3H	;行の先頭でないなら改行
+CR1		EQU	04A7H	;改行
+ACCDIS		EQU	04C8H	;１文字出力 コントロール・コード表示版
+MONOP		EQU	0FE2H	;モニタコールドスタート
+HOTSTA		EQU	1003H	;モニタホットスタート
+ACHXPR		EQU	1207H	;Aレジスタの値を16進数で出力
+TUPPER		EQU	1451H	;大文字に変換
+IPLFCB		EQU	1480H	;CZ-8CB01
+;FNAME		EQU	IPLFCB+1
+;FSIZE		EQU	IPLFCB+18
+;SADRS		EQU	IPLFCB+20
+;EXEAD		EQU	IPLFCB+22
+
+
+		ORG	14A0H
+
+		LD	DE,TITLE			;'******** EMMLOAD,EMMSAVE MENU ********'表示
+		CALL	PRINT
+		CALL	CR1
+MENU:		CALL	CR1
+		LD	DE,EMENU			;'0)EMM0 1)EMM1 2)EMM2 3)EMM3 ?'表示
+		CALL	PRINT
+		LD	A,01H				;一文字入力,キー入力待ち
+		CALL	INKEYD
+		LD	(LNAME+3),A			;EMM0～EMM3の文字を変更
+		LD	(MSG1+3),A
+		LD	(SNAME+3),A
+		LD	(MSG4+3),A
+
+		CP	'0'				;EMM0
+		JR	NZ,EMM1
+		LD	HL,0D00H			;EMM I/Oアドレス 0D00H
+		LD	(EMMADRS),HL
+		JR	MENUS
+EMM1:		CP	'1'				;EMM1
+		JR	NZ,EMM2
+		LD	HL,0D04H			;EMM I/Oアドレス 0D04H
+		LD	(EMMADRS),HL
+		JR	MENUS
+EMM2:		CP	'2'				;EMM2
+		JR	NZ,EMM3
+		LD	HL,0D08H			;EMM I/Oアドレス 0D08H
+		LD	(EMMADRS),HL
+		JR	MENUS
+EMM3:		CP	'3'				;EMM3
+		JR	NZ,MENU
+		LD	HL,0D0CH			;EMM I/Oアドレス 0D0CH
+		LD	(EMMADRS),HL
+		
+MENUS:		CALL	CR1
+		LD	DE,SMENU			;'1)EMMLOAD 2)EMMSAVE 3)EXIT ?'表示
+		CALL	PRINT
+		LD	A,01H				;一文字入力,キー入力待ち
+		CALL	INKEYD
+		CP	'1'
+		JR	Z,LSTART			;EMMLOAD
+		CP	'2'
+		JP	Z,SSTART			;EMMSAVE
+		CP	'3'
+		JR	NZ,MENU
+EXIT:		CALL	CR1
+		LD	DE,ENDMSG			;'Run Again:[G 14A0],If not:[IPL RESET]'表示
+		CALL	PRINT
+		JP	HOTSTA
+
+;**************************** EMMLOAD *********************************
+LSTART:
+		CALL	CR1
+		LD	DE,MSG3				;「FNAME:」を表示する
+		CALL	PRINT
+		LD	DE,BUF				;DOSファイル名を入力
+		CALL	INPUTF
+		JP	C,EXIT				;BREAKキーが押されたらキャンセル
+		LD	DE,BUF+6			;DOSファイル名をCHECK
+		LD	A,(DE)
+ST1:		CP	0DH				;入力なしならDefaultの「EMM0」～「EMM3」を採用
+		JR	Z,ST0
+		CP	00H				;入力なしならDefaultの「EMM0」～「EMM3」を採用
+		JR	Z,ST0
+		CP	' '				;SPACEは読み飛ばし
+		JR	NZ,ST2
+		INC	DE
+		JR	ST1
+ST2:		CP	'*'				;*が入力されたら「*F」処理へ
+		JR	Z,ST3
+		LD	(ML0+1),DE			;DOSファイル名が入力されていたらそちらを採用
+		LD	(ML1+1),DE
+		JR	ST0
+
+;**************************** アプリケーション内SD-CARD操作処理 **********************
+ST3:
+MLHCMD:
+		INC	DE
+		INC	DE
+
+FC0:		
+		LD	A,(DE)				;スペース読み飛ばし
+		CP	00H
+		JR	Z,FC2
+		INC	DE
+		CP	20H
+		JR	NZ,FC1
+		JR	FC0
+FC1:		DEC	DE
+		DEC	DE
+FC2:
+
+		LD	HL,MSG3   		      ;行頭に'FNAME:'を付けることでカーソルを移動させリターンで実行できるように
+		LD	BC,MSG3END-MSG3
+;**** FDLコマンド呼び出し ****
+		CALL	DIRLIST
+		AND	A          ;00以外ならERROR
+		JR	NZ,SERR
+;**** ファイルネーム入力へ復帰 ****
+		JP	LSTART
+
+;******* アプリケーション内SD-CARD操作処理用ERROR処理 **************
+SERR:
+		CP	0F0H
+		JR	NZ,SERR3
+		LD	DE,MSG_F0
+		JR	SERRMSG
+		
+SERR3:		CP		0F1H
+		JR	NZ,SERR99
+		LD	DE,MSG_F1
+		JR	SERRMSG
+		
+SERR99:		CALL	ACHXPR
+		LD	DE,MSG99
+		
+SERRMSG:
+		CALL	PRINT
+		CALL	CR1
+		POP	BC
+		POP	DE
+		POP	HL
+;**** ファイルネーム入力へ復帰 ****
+		JP	LSTART
+
+;********************************** EMMLOAD処理本体 ************************************
+ST0:		CALL	CR1
+ML1:		LD	DE,LNAME			;DOSファイル名表示
+		CALL	PRINT
+		LD	DE,MSG0				;' LOAD START!'表示
+		CALL	PRINT
+
+		CALL	EMMRESET			;EMMアドレスリセット
+
+		LD	HL,IPLFCB
+		LD	BC,0020H
+		CALL	MLHED				;DOSファイル名をArduinoに送信してファイルオープン
+		JP	C,EXIT				;DOSファイル名のファイルが存在しない等のエラーがあれば中止
+		
+		LD	B,10H				;SDからのLOADを8000H(32KB)を16回繰り返し
+		LD	C,30H				;ブロック名として0～Fを便宜的に表示
+		
+LOP2:		PUSH	BC
+		CALL	BPRNT
+		LD	DE,MSG2				;' BLOCK LOADING'を表示
+		CALL	PRINT
+
+		LD	HL,BUFF
+		LD	BC,8000H
+		CALL	MLDAT				;Arduinoから1ブロックを読み込み
+		
+		LD	HL,BUFF
+		LD	DE,8000H
+		LD	BC,(EMMADRS)			;EMM I/Oアドレス設定
+LOP1:		
+		LD	A,(HL)
+		OUT	(C),A				;BUFFから8000H(32KB)を1Byteずつ読み込み、EMMへ書き込み
+		INC	HL
+		DEC	DE
+		LD	A,E
+		OR	D
+		JR	NZ,LOP1				;32KB分ループ
+
+		POP	BC
+		INC	C
+		DJNZ	LOP2				;16回ループ
+		
+		CALL	CR1
+		LD	DE,MSG1				;'EMMx LOAD END'を表示
+		CALL	PRINT
+		CALL	CR1
+		JP	EXIT				;終了
+
+;************************ セットされたDOSファイル名でファイルオープンするためのMLHED処理 **************
+MLHED:
+		DI
+		PUSH	DE
+		PUSH	BC
+		PUSH	HL
+		LD	(FCB),HL
+		LD	(FSIZE),BC
+ML0:		LD	DE,LNAME
+		JP	MLHED2
+
+
+
+
+;******************** EMMSAVE ********************************
+
+SSTART:
+		CALL	CR1
+		CALL	CR1
+		LD	DE,SNAME			;DOSファイル名表示
+		CALL	PRINT
+		LD	DE,MSG6				;' SAVE START!'表示
+		CALL	PRINT
+
+		CALL	EMMRESET			;EMMアドレスリセット
+
+		LD	A,01H				;IFBにファイル種別をセット(実は値が何でもOK)
+		LD	(IPLFCB),A
+		LD	HL,SNAME			;IFBにファイル名をセット(実は値が何でもOK)
+		LD	DE,IPLFCB+1
+		LD	BC,10H
+		LDIR
+		
+		LD	HL,IPLFCB
+		LD	BC,0020H
+		CALL	MSHED				;ArduinoにIFBを送信、DOSファイルオープン
+		
+		LD	HL,8000H			;IFBにファイル長(8000H)をセット
+		LD	(FSIZE),HL
+		LD	HL,BUFF
+		LD	(SADRS),HL			;IFBにメインメモリ格納先頭アドレス(BUFF)をセット
+		LD	B,10H				;EMMから8000H(32KB)分読み出しを16回繰り返し
+		LD	C,30H				;ブロック名として0～Fを便宜的に表示
+		
+LOP3:		PUSH	BC
+
+		CALL	BPRNT
+		LD	DE,MSG5				;' BLOCK SAVEING'を表示
+		CALL	PRINT
+
+		LD	HL,BUFF
+		LD	DE,8000H
+		LD	BC,(EMMADRS)			;EMM I/Oアドレス設定
+LOP4:		
+		IN	A,(C)				;EMMから8000H(32KB)分読み出してBUFFへ書き込み
+		LD	(HL),A
+		INC	HL
+		DEC	DE
+		LD	A,E
+		OR	D
+		JR	NZ,LOP4				;32KB分ループ
+		
+		LD	HL,BUFF
+		LD	BC,8000H
+		CALL	MSDAT				;Arduinoへ1ブロックを送信し、SDへ書き込み
+
+		POP	BC
+		INC	C
+		DJNZ	LOP3				;16回ループ
+		
+		CALL	CR1
+		LD	DE,MSG4				;'EMMx SAVE END'を表示
+		CALL	PRINT
+		CALL	CR1
+		JP	EXIT				;終了
+
+
+EMMRESET:
+		XOR	A				;EMMアドレスリセット
+		LD	BC,(EMMADRS)
+		OUT	(C),A
+		INC	BC
+		OUT	(C),A
+		INC	BC
+		OUT	(C),A
+		INC	BC
+		LD	(EMMADRS),BC
+		RET
+
+BPRNT:
+		CALL	CR1
+		LD	A,C				;ブロック名としてA～Fを表示するための処理
+		CP	3AH
+		JR	C,ALPHA
+		ADD	A,07H
+ALPHA:		CALL	ACCPRT
+		RET
+
+TITLE:
+		DB	'******** EMMLOAD,EMMSAVE MENU ********'
+		DB	00H
+
+EMENU:		DB	'0)EMM0 1)EMM1 2)EMM2 3)EMM3 ?'
+		DB	00H
+
+SMENU:		DB	'1)EMMLOAD 2)EMMSAVE 3)EXIT ?'
+		DB	00H
+
+ENDMSG:		DB	'Run Again:[G 14A0],If not:[IPL RESET]'
+		DB	00H
+
+EMMADRS:	DW	0D00H
+
+LNAME:		DB	'EMM0'
+		DB	00H
+LNAME_END:
+
+SNAME:		DB	'EMM0-SAVE'
+		DB	00H
+SNAME_END:
+
+MSG0:		DB	' LOAD START!'
+		DB	00H
+
+MSG1:
+		DB	'EMM0 LOAD END'
+		DB	00H
+
+MSG2:		DB	' BLOCK LOADING'
+		DB	00H
+		
+MSG3:		DB	'FNAME:'
+MSG3END:
+		DB	00H
+
+MSG4:
+		DB	'EMM0 SAVE END'
+		DB	00H
+
+MSG5:		DB	' BLOCK SAVEING'
+		DB	00H
+
+MSG6:		DB	' SAVE START!'
+		DB	00H
+
+
+
+;**** 1BYTE受信 ****
+;受信DATAをAレジスタにセットしてリターン
+RCVBYTE:
+		PUSH	BC
+		CALL	F1CHK     			;PORTC BIT7が1になるまでLOOP
+		LD	BC,007DH
+		IN	A,(C)				;PORTB -> A
+		PUSH 	AF
+		LD	A,05H
+		LD	BC,007FH
+		OUT	(C),A				;PORTC BIT2 <- 1
+		CALL	F2CHK				;PORTC BIT7が0になるまでLOOP
+		LD	A,04H
+		LD	BC,007FH
+		OUT	(C),A				;PORTC BIT2 <- 0
+		POP 	AF
+		POP	BC
+		RET
+
+;**** BUSYをCHECK(1) ****
+; 7EH BIT7が1になるまでLOP
+F1CHK:		LD	BC,007EH
+		IN	A,(C)
+		AND	80H				;PORTC BIT7 = 1?
+		JR	Z,F1CHK
+		RET
+
+;**** BUSYをCHECK(0) ****
+; 7EH BIT7が0になるまでLOOP
+F2CHK:		LD	BC,007EH
+		IN	A,(C)
+		AND	80H				;PORTC BIT7 = 0?
+		JR	NZ,F2CHK
+		RET
+
+;**** 1BYTE送信 ****
+;Aレジスタの内容をPORTA下位4BITに4BITずつ送信
+SNDBYTE:
+		PUSH	BC
+		PUSH	AF
+		RRA
+		RRA
+		RRA
+		RRA
+		AND	0FH
+		CALL	SND4BIT
+		POP	AF
+		AND	0FH
+		CALL	SND4BIT
+		POP	BC
+		RET
+
+;**** 4BIT送信 ****
+;Aレジスタ下位4ビットを送信する
+SND4BIT:
+		LD	BC,007CH
+		OUT	(C),A
+		LD	A,05H
+		LD	BC,007FH
+		OUT	(C),A				;PORTC BIT2 <- 1
+		CALL	F1CHK				;PORTC BIT7が1になるまでLOOP
+		LD	A,04H
+		LD	BC,007FH
+		OUT	(C),A				;PORTC BIT2 <- 0
+		CALL	F2CHK
+		RET
+		
+
+;*********************** MSHED ライト インフォメーション代替処理 ************
+MSHED:
+		DI
+		PUSH	DE
+		PUSH	BC
+		PUSH	HL
+		LD	(FSIZE),BC
+		LD	(FCB),HL
+		LD	A,91H				;HEADER SAVEコマンド91H
+		CALL	MCMD				;コマンドコード送信
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+		LD	HL,(FCB)
+		LD	BC,(FSIZE)
+		LD	B,C
+MSH3:		LD	A,(HL)				;インフォメーション ブロック送信
+		CALL	SNDBYTE
+		INC	HL
+		DEC	B
+		JR	NZ,MSH3
+
+		CALL	RCVBYTE				;状態取得(00H=OK)
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+		JP	MRET				;正常RETURN
+
+;******************** MSDAT ライト データ代替処理 **********************
+MSDAT:
+		DI
+		PUSH	DE
+		PUSH	BC
+		PUSH	HL
+		LD	(SADRS),HL
+		LD	(FSIZE),BC
+		LD	A,92H				;DATA SAVEコマンド92H
+		CALL	MCMD				;コマンドコード送信
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+		
+		LD	HL,FSIZE			;FSIZE送信
+		LD	A,(HL)
+		CALL	SNDBYTE
+		INC	HL
+		LD	A,(HL)
+		CALL	SNDBYTE
+
+		CALL	RCVBYTE				;状態取得(00H=OK)
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+		LD	DE,(FSIZE)
+		LD	HL,(SADRS)
+MSD1:		LD	A,(HL)
+		CALL	SNDBYTE				;SADRSからFSIZE Byteを送信。分割セーブの場合、直前に0436HでOPENされたファイルを対象として256バイトずつ0475HがCALLされる。
+		DEC	DE
+		LD	A,D
+		OR	E
+		INC	HL
+		JR	NZ,MSD1
+		
+		JP	MRET				;正常RETURN
+
+;************************** MLHED リード インフォメーション代替処理 *****************
+
+MLHED2:		LD	A,93H				;HEADER LOADコマンド93H
+		CALL	MCMD				;コマンドコード送信
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+MLH1:
+		LD	A,(DE)
+		CP	20H				;行頭のスペースをファイルネームまで読み飛ばし
+		JR	NZ,MLH2
+		INC	DE
+		JR	MLH1
+
+MLH2:		LD	B,20H
+MLH4:		LD	A,(DE)				;FNAME送信
+		CALL	SNDBYTE
+		INC	DE
+		DEC	B
+		JR	NZ,MLH4
+		LD	A,0DH
+		CALL	SNDBYTE
+		
+		CALL	RCVBYTE				;状態取得(00H=OK)
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+		CALL	RCVBYTE				;状態取得(00H=OK)
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+		LD	HL,(FCB)
+		LD	BC,(FSIZE)
+		LD	B,C
+MLH5:		CALL	RCVBYTE				;読みだされたインフォメーションブロックを受信
+		LD	(HL),A
+
+		INC	HL
+		DEC	B
+		JR	NZ,MLH5
+
+		CALL	RCVBYTE				;状態取得(00H=OK)
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+		JP	MRET				;正常RETURN
+
+;**************************** MLDAT リード データ代替処理 ********************
+MLDAT:
+		DI
+		PUSH	DE
+		PUSH	BC
+		PUSH	HL
+		LD	(SADRS),HL
+		LD	(FSIZE),BC
+		LD	A,94H				;DATA LOADコマンド94H
+		CALL	MCMD				;コマンドコード送信
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+		CALL	RCVBYTE				;状態取得(00H=OK)
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+		CALL	RCVBYTE				;状態取得(00H=OK)
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+		LD	DE,FSIZE			;FSIZE送信
+		LD	A,(DE)
+		CALL	SNDBYTE
+		INC	DE
+		LD	A,(DE)
+		CALL	SNDBYTE
+		CALL	DBRCV2				;FSIZE分のデータを受信し、SADRSから格納。分割ロードの場合、直前に0436HでOPENされたファイルを対象として256バイトずつSADRSが加算されて04F8HがCALLされる。
+
+		CALL	RCVBYTE				;状態取得(00H=OK)
+		AND	A				;00以外ならERROR
+		JP	NZ,MERR
+
+		JR	MRET				;正常RETURN
+
+;データ受信2
+DBRCV2:		LD	DE,(FSIZE)
+		LD	HL,(SADRS)
+DBRLP2:		CALL	RCVBYTE
+		LD	(HL),A
+		DEC	DE
+		LD	A,D
+		OR	E
+		INC	HL
+		JR	NZ,DBRLP2			;DE=0までLOOP
+		RET
+
+;******* 代替処理用コマンドコード送信 (IN:A コマンドコード) **********
+MCMD:
+		CALL	SNDBYTE				;コマンドコード送信
+		CALL	RCVBYTE				;状態取得(00H=OK)
+		RET
+
+;****** 代替処理用正常RETURN処理 **********
+MRET:		POP	HL
+		POP	BC
+		POP	DE
+		XOR	A				;正常終了フラグ
+		
+		RET
+
+;******* 代替処理用ERROR処理 **************
+MERR:
+		CP	0F0H
+		JR	NZ,MERR3
+		LD	DE,MSG_F0
+		JR	MERRMSG
+		
+MERR3:		CP	0F1H
+		JR	NZ,MERR99
+		LD	DE,MSG_F1
+		JR	MERRMSG
+		
+MERR99:		CALL	ACHXPR
+		LD	DE,MSG99
+		
+MERRMSG:
+		CALL	PRINT
+		CALL	CR1
+		POP	HL
+		POP	BC
+		POP	DE
+		LD	A,02H
+		SCF
+
+		RET
+
+DEFDIR:
+		DB	'*L '
+DEND:
+
+;**** DIRLIST本体 (HL=行頭に付加する文字列の先頭アドレス BC=行頭に付加する文字列の長さ) ****
+;****              戻り値 A=エラーコード ****
+DIRLIST:
+		LD	A,83H				;DIRLISTコマンド83Hを送信
+		CALL	STCD				;コマンドコード送信
+		AND	A				;00以外ならERROR
+		JP	NZ,DLRET
+		
+		PUSH	BC
+		LD	B,21H
+STLT1:		LD	A,(DE)
+		CALL	TUPPER
+STLT2:		CALL	SNDBYTE				;比較文字列を送信
+		INC	DE
+		DJNZ	STLT1
+		POP	BC
+DL1:
+		PUSH	HL
+		PUSH	BC
+		LD	DE,BUF
+		LDIR
+		EX	DE,HL
+DL2:		CALL	RCVBYTE				;'00H'を受信するまでを一行とする
+		CP	00H
+		JR	Z,DL3
+		CP	0FFH				;'0FFH'を受信したら終了
+		JR	Z,DL4
+		CP	0FEH				;'0FEH'を受信したら一時停止して一文字入力待ち
+		JR	Z,DL5
+		LD	(HL),A
+		INC	HL
+		JR	DL2
+DL3:		LD	(HL),A
+		LD	DE,BUF				;'00H'を受信したら一行分を表示して改行
+		CALL	PRINT
+		CALL	CR2
+		POP	BC
+		POP	HL
+		JR	DL1
+DL4:		CALL	RCVBYTE				;状態取得(00H=OK)
+		POP	BC
+		POP	HL
+		JR	DLRET
+
+DL5:		LD	DE,MSG_KEY1			;HIT ANT KEY表示
+		CALL	PRINT
+		LD	A,01EH
+		CALL	ACCDIS
+		LD	DE,MSG_KEY2			;HIT ANT KEY表示
+		CALL	PRINT
+		CALL	CR2
+DL6:		LD	A,01H
+		CALL	INKEYD				;1文字入力待ち
+		CALL	TUPPER
+		CP	00H
+		JR	Z,DL6
+		CP	03H				;SHIFT+BREAKで打ち切り
+		JR	Z,DL7
+		CP	1BH				;ESCで打ち切り
+		JR	Z,DL7
+		CP	1EH				;カーソル↑で打ち切り
+		JR	Z,DL9
+		CP	42H				;「B」で前ページ
+		JR	Z,DL8
+		LD	A,00H				;それ以外で継続
+		JR	DL8
+DL9:		LD	A,01EH				;カーソル↑で打ち切ったときにカーソル2行上へ
+		CALL	ACCPRT
+		LD	A,01EH
+		CALL	ACCPRT
+DL7:		LD	A,0FFH				;0FFH中断コードを送信
+DL8:		CALL	SNDBYTE
+		CALL	CR2
+		JR	DL2
+		
+DLRET:		
+		RET
+		
+MSG_DNAME:
+		DB	'DOS FILE:'
+MSG_DNAMEEND:
+		DB	'                            '
+		DB	00H
+		
+MSG_KEY1:
+		DB	'NEXT:ANY BACK:B BRK:'
+		DB	00H
+MSG_KEY2:
+		DB	' or ESC or SHT+BRK'
+		DB	00H
+		
+MSG_LD:
+		DB	'LOADING '
+		DB	00H
+
+WRMSG:
+		DB	'WRITEING '
+		DB	00H
+
+MSG_FNAME:
+		DB	'FILE NAME FAILED!'
+		DB	00H
+		
+MSG_CMD:
+		DB	'COMMAND FAILED!'
+		DB	00H
+		
+MSG_F0:
+		DB	'SD-CARD INITIALIZE ERROR'
+		DB	00H
+		
+MSG_F1:
+		DB	'NOT FIND FILE'
+		DB	00H
+		
+MSG_F3:
+		DB	'FILE EXIST'
+		DB	00H
+		
+MSG99:
+		DB	' ERROR'
+		DB	00H
+		
+;**** コマンド送信 (IN:A コマンドコード)****
+STCD:		CALL	SNDBYTE				;Aレジスタのコマンドコードを送信
+		CALL	RCVBYTE				;状態取得(00H=OK)
+		RET
+
+;**** コマンド、ファイル名送信 (IN:A コマンドコード HL:ファイルネームの先頭)****
+STCMD:		CALL	STFN				;ファイルネーム取得
+		PUSH	HL
+		CALL	STCD				;コマンドコード送信
+		POP	HL
+		AND	A				;00以外ならERROR
+		JP	NZ,SVER0
+		CALL	STFS				;ファイルネーム送信
+		AND	A				;00以外ならERROR
+		JP	NZ,SVER0
+		RET
+
+;****** FILE NAME 取得 (IN:DE コマンド文字の次の文字 OUT:HL ファイルネームの先頭)*********
+STFN:		PUSH	AF
+STFN1:		INC	DE				;ファイルネームまでスペース読み飛ばし
+		LD	A,(DE)
+		CP	20H
+		JR	Z,STFN1
+		CP	30H				;「0」以上の文字でなければエラーとする
+		JP	C,STSV2
+		EX	DE,HL
+		POP	AF
+		RET
+
+;**** ファイルネーム送信(IN:HL ファイルネームの先頭) ******
+STFS:		LD	B,20H
+STFS1:		LD	A,(HL)				;FNAME送信
+		CALL	SNDBYTE
+		INC	HL
+		DEC	B
+		JR	NZ,STFS1
+		LD	A,0DH
+		CALL	SNDBYTE
+		CALL	RCVBYTE				;状態取得(00H=OK)
+		RET
+
+STSV2:							;ファイルネームの取得に失敗
+		LD	DE,MSG_FNAME
+		JR	ERRMSG
+SVER0:
+		POP	DE				;CALL元STACKを破棄する
+SVERR:
+		CP	0F0H
+		JR	NZ,ERR3
+		LD	DE,MSG_F0			;SD-CARD INITIALIZE ERROR
+		JR	ERRMSG
+ERR3:		CP	0F1H
+		JR	NZ,ERR4
+		LD	DE,MSG_F1			;NOT FIND FILE
+		JR	ERRMSG
+ERR4:		CP	0F3H
+		JR	NZ,ERR5
+		LD	DE,MSG_F3			;FILE EXIST
+		JR	ERRMSG
+ERR5:		CP	0F4H
+		JR	NZ,ERR99
+		LD	DE,MSG_CMD
+		JR	ERRMSG
+ERR99:		CALL	ACHXPR
+		LD	DE,MSG99			;その他ERROR
+ERRMSG:		CALL	PRINT
+		CALL	CR2
+MON:		RET
+
+ENT6:
+
+
+FCB		DS	2
+FSIZE		DS	2
+SADRS		DS	2
+BUF:		DS	80
+
+BUFF:
+		DS	8000H
+			END
---
+++ SAMPLE/EMMMENU/README.md
@@ -0,0 +1,104 @@
+# EMMMENU(SDからEMMへLOAD、EMMからSDへSAVE) for X1_SD
+
+X1_SDを使い、EMMボートの512K S-RAMの内容をまるごとSDへSAVE、またSDにSAVEされたEMMの内容をまるごとEMMボートへLOADするプログラムです。
+
+X1、X1turboで実行可能です。
+
+EMMSAVEでSDカードに保存されるEMM DUMPファイルはX1Tファイルのヘッダが付加され512KB+32Byte=524320Byteになります。
+
+なお、EMM DUMPファイルはWindowsパソコンのエミュレーター2種類で利用可能であることを確認しています。
+
+## 利用可能なエミュレーター
+
+### TAKEDAさんのeX1, eX1twin, eX1turbo, eX1turboZ
+
+https://takeda-toshiya.my.coocan.jp/
+
+　EMM DUMPファイルから32Byteのヘッダを除去し、EMM.BINにリネームしてエミュレーターと同じフォルダに置けばエミュレーターから利用できるようになります。
+
+　eX1, eX1twin, eX1turbo, eX1turboZは、16MのEMM0として対応しているようです。EMM DUMPファイルから32Byteのヘッダを除去した524320Byte、327680Byte(Hu-BASICから認識可能分)のEMM.BINでも問題なく認識しますが、エミュレーターを終了させると16MByteのファイルとして再作成されます。
+
+　16MByteとして作成されたEMM.BINに32Byteのヘッダをつけて拡張子をX1TとすればEMM DUMPファイルとしてEMMLOADが可能です。(LOADされるのは先頭512kByte分のみ)
+
+　この時、付加する32Byteのヘッダの内容はどんな内容でも構わないので00hを32Byte等好きにつけてください。
+
+### X millennium系
+
+https://www.turboz.to/
+
+　EMM DUMPファイルから32Byteのヘッダを除去し、EMMx.MEM(EMM0.MEM、EMM1.MEM、EMM2.MEM、EMM3.MEM)にリネームしてエミュレーターと同じフォルダに置けばエミュレーターから利用できるようになります。
+
+　320KB EMMしかサポートしていない気がしますが、エミュレーター終了時に書き出されるときにファイルサイズは変化しないようです。
+
+　EMMx.MEM(EMM0.MEM、EMM1.MEM、EMM2.MEM、EMM3.MEM)に32Byteのヘッダをつけて拡張子をX1TとすればEMM DUMPファイルとしてEMMLOADが可能です。ファイル長が320KB+32BytのときもEMMLOADにそのまま使えます。
+
+　付加する32Byteのヘッダの内容はどんな内容でも構わないので00hを32Byte等好きにつけてください。
+
+## EMMMENUの作成
+　EMMMENU.binを使いますが、BASIC CZ-8CB01から抜き出したIOCS(0000h～149Fh)が必要です。
+
+　基本的にはX1_SDでの「ROMへの書込み」と同様です。
+
+　まず、BASIC CZ-8CB01からIOCS(0000h～149Fh)を抜き出し、バイナリファイルとして保存します。
+
+　CZ-8FB01 V10、CZ-8FB01 V20も大丈夫だとは思いますが、検証していません。なお、X1turbo用BASICは使えません。
+
+　　DumpList Editorを使ってBASIC CZ-8CB01のWav又はtapファイルを読み込み、「マシン語入力に送る」を実行します。
+
+　　マシン語の14A0h以降を削除します。(0000h～149Fhのマシン語にします)
+
+　　「ファイル書出し」の「BIN(mot)ファイル書き出し(ヘッダ無し)(for他汎用)」を選択して「IOCS.bin」という名前で保存します。
+
+　保存した「IOCS.bin」の後ろにバイナリエディタ等を使って「EMMMENU.bin」を付け加えます。0000h～1AD6hのファイルとなるはずです。
+
+　次にファイルの先頭に以下に示す32Byteのヘッダを付加します。0000h～1AF6hのファイルとなります。
+
+　[0000] 01 45 4D 4D 4D 45 4E 55 20 20 20 20 20 20 20 20
+
+　[0010] 20 20 D6 1A 00 00 00 00 20 20 20 20 20 20 00 00
+
+　出来上がったら「EMMMENU.X1T」という名前で保存します。
+
+## EMMMENUの使い方
+　起動するとまず対象とするEMMをEMM0～EMM3の中から数字で選択します。
+
+　次にEMMLOAD、EMMSAVE、EXITを数字で選択します。
+
+　EXIT及びEMMLOAD、EMMSAVE終了時に別のプログラムを実行したいときはIPLリセットしてください。
+
+### 1)EMMLOAD
+　選択すると「FNAME:」と聞いてきますので次のうちから選択して入力します。
+
+### [BREAK]キー
+　処理が中断されたものとしてEXIT処理になります。
+
+### DOSファイル名を入力せずそのまま[CR]キー
+　選択したEMMに対応するEMMx.X1T(EMM0.X1T、EMM1.X1T、EMM2.X1T、EMM3.X1T)をDOSファイル名としてSDカードから選択したEMMに転送します。
+
+### DOSファイル名を入力して[CR]キー
+　入力されたDOSファイル名のファイルをSDカードから選択したEMMに転送します。拡張子の「X1T」は省略可能です。
+
+### *F xxx[CR]キー
+　SDカードに保存されている文字列xxxから始まるDOSファイル名を一覧表示します。xxxを省略した場合はすべてのファイルが一覧表示対象となります。
+
+　20件表示したところで指示待ちになるので打ち切るならSHIFT+BREAK又は↑を入力すると打ち切られ、Bキーで前の20件に戻ります。それ以外のキーで次の20件を表示します。
+
+　検索結果の行頭には「FNAME:」を付加して表示してあるのでLOADしたいファイルにカーソルキーを合わせて[CR]キーを押すだけでLOADが可能です。
+
+　SDからEMMへの転送にかかる時間は約120秒です。
+
+例)
+
+FNAME:[CR]
+
+FNAME:EMM0-TEST[CR]
+
+FNAME:*FDL EMM[CR]
+
+
+### 2)EMMSAVE
+　選択すると先に選択していたEMMによりEMMx-SAVE.X1T(EMM0-SAVE.X1T、EMM1-SAVE.X1T、EMM2-SAVE.X1T、EMM3-SAVE.X1T)というファイルがSDカードに作成されます。
+
+　同名のファイルがあると上書きされます。SDに保存されるファイル名はEMMx-SAVE.X1T(EMM0-SAVE.X1T、EMM1-SAVE.X1T、EMM2-SAVE.X1T、EMM3-SAVE.X1T)に固定されていて指定はできません。
+
+　EMMからSDへの保存にかかる時間は約150秒です。
